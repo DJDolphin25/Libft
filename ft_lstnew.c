7deb20d10e08fd05ab39626e6752b3831d11476e
Theophilus Kofi Oppong Ansah <theoppon@mi-r1-p6.s19.be>
2025-11-21 17:11:32 +0100

    i need to finish the bonus

---
+++ ft_lstnew.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: theoppon <theoppon@student.42belgium.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/16 11:47:03 by theoppon          #+#    #+#             */
+/*   Updated: 2025/11/16 13:51:43 by theoppon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+int main()
+{
+	int	*num;
+
+	&num = 45;
+	t_list	*node = ft_lstnew(num)
+}
+
